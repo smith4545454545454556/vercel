5b1fed642ce2a3df744c0ccc0c957e18649882c1
smith4545454545454556 <smith.maharjan1233@gmail.com>
2024-11-26 15:07:39 +0545

    frontend url

--- vercel_backend/app.js
+++ vercel_backend/app.js
@@ -1,9 +1,10 @@
 import express, { json } from "express"
 import cors from "cors"
 import { router } from "./src/route/dataRoutes.js";
+import dotenv from "dotenv"
 const app = express()
 const corsOptions = {
-    origin: "http://localhost:5173", // Allow your frontend
+    origin: process.env.FRONTEND_ORIGIN, // Allow your frontend
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],  // Allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,              // Allow sending cookies or Authorization header
