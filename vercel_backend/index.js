2cf20b5bdc310e547be4b3fbe91d315c15f3d867
smith4545454545454556 <109470178+smith4545454545454556@users.noreply.github.com>
2024-11-26 16:53:58 +0545

    Update index.js

--- vercel_backend/index.js
+++ vercel_backend/index.js
@@ -1,7 +1,9 @@
 import app from "./app.js";
 import { port } from "./src/config/config.js";
 import { connectToDB } from "./src/config/database.js";
-app.listen(port, async () => {
+const PORT = process.env.PORT || 3100;
+
+app.listen(PORT, async () => {
     await connectToDB()
     console.log("server is running")
 })
